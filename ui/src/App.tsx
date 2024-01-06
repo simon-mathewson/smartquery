991772daeb9f1e393df5346dfb827362d104b2c2
Simon Mathewson <info@simonmathewson.com>
2024-01-06 15:55:43 +0100

    Add logo

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -114,24 +114,31 @@ export const App: React.FC = () => {
         setSelectedDatabase,
       }}
     >
-      <Sidebar />
-      <div className="h-full overflow-hidden bg-white pl-[224px]">
-        <div className="flex h-full justify-start overflow-hidden">
-          <DropMarker column={0} row={0} />
-          {queries.map((column, columnIndex) => (
-            <React.Fragment key={columnIndex}>
-              <div className="flex w-full flex-col justify-start overflow-hidden">
-                <DropMarker column={columnIndex} horizontal row={0} />
-                {column.map((query, rowIndex) => (
-                  <React.Fragment key={query.id}>
-                    <Query columnIndex={columnIndex} query={query} rowIndex={rowIndex} />
-                    <DropMarker column={columnIndex} horizontal row={rowIndex + 1} />
-                  </React.Fragment>
-                ))}
-              </div>
-              <DropMarker column={columnIndex + 1} row={0} />
-            </React.Fragment>
-          ))}
+      <div className="grid grid-cols-[224px_1fr]">
+        <Sidebar />
+        {queries.length === 0 && (
+          <div className="flex items-center justify-center">
+            <img className="w-50 h-max opacity-20" src="/LogoIcon.svg" />
+          </div>
+        )}
+        <div className="h-full overflow-hidden bg-white">
+          <div className="flex h-full justify-start overflow-hidden">
+            <DropMarker column={0} row={0} />
+            {queries.map((column, columnIndex) => (
+              <React.Fragment key={columnIndex}>
+                <div className="flex w-full flex-col justify-start overflow-hidden">
+                  <DropMarker column={columnIndex} horizontal row={0} />
+                  {column.map((query, rowIndex) => (
+                    <React.Fragment key={query.id}>
+                      <Query columnIndex={columnIndex} query={query} rowIndex={rowIndex} />
+                      <DropMarker column={columnIndex} horizontal row={rowIndex + 1} />
+                    </React.Fragment>
+                  ))}
+                </div>
+                <DropMarker column={columnIndex + 1} row={0} />
+              </React.Fragment>
+            ))}
+          </div>
         </div>
       </div>
     </GlobalContext.Provider>
