6e4b0d06e8181162d47a5d699709edd5c31c80b8
Simon Mathewson <simon.mathewson@cosuno.de>
2024-05-19 00:15:47 +0200

    Link: Fix build, implement tray, update icons

--- link/src/main/index.ts
+++ link/src/main/index.ts
@@ -1,13 +1,22 @@
 import { electronApp } from '@electron-toolkit/utils';
-import { app } from 'electron';
+import { app, Menu, Tray, nativeImage } from 'electron';
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
 import cors from 'cors';
 import { router } from './router';
+import settings from 'electron-settings';
+import iconPath from '../../resources/trayIconTemplate.png?asset';
 
 app.whenReady().then(() => {
   // Set app user model ID for Windows
   // https://learn.microsoft.com/en-us/windows/win32/shell/appids
-  electronApp.setAppUserModelId('com.dabase');
+  electronApp.setAppUserModelId('dev.dabase.link');
+
+  // Hide the dock icon on macOS
+  if (process.platform === 'darwin') {
+    app.dock.hide();
+  }
+
+  createTray();
 
   const server = createHTTPServer({
     middleware: cors(),
@@ -18,3 +27,37 @@ app.whenReady().then(() => {
   server.listen(3500);
   console.log('Link listening on port 3500');
 });
+
+const createTray = () => {
+  const icon = nativeImage.createFromPath(iconPath);
+
+  const tray = new Tray(icon);
+
+  tray.setToolTip('Dabase Link');
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Start on Login',
+      type: 'checkbox',
+      checked: (settings.getSync('startOnLogin') as boolean | undefined) ?? false,
+      click: (menuItem) => {
+        const newValue = menuItem.checked;
+        settings.setSync('startOnLogin', newValue);
+        toggleAutoLaunch(newValue);
+
+        // The context menu closes by default, but we want to keep it open
+        tray.popUpContextMenu();
+      },
+    },
+    { type: 'separator' },
+    {
+      label: 'Quit',
+      click: app.quit,
+    },
+  ]);
+  tray.setContextMenu(contextMenu);
+};
+
+const toggleAutoLaunch = (enable: boolean) => {
+  app.setLoginItemSettings({ openAtLogin: enable });
+};
