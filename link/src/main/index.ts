9ce0f5c08f8cd0819c456f1c77d1a4f92863b4b0
Simon Mathewson <simon.mathewson@cosuno.de>
2024-06-07 22:57:20 +0200

    Move `createTray` out of main file

--- link/src/main/index.ts
+++ link/src/main/index.ts
@@ -1,11 +1,9 @@
 import { electronApp } from '@electron-toolkit/utils';
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
 import cors from 'cors';
-import type { MenuItemConstructorOptions } from 'electron';
-import { Menu, Tray, app, nativeImage } from 'electron';
-import settings from 'electron-settings';
-import iconPath from '../../resources/trayIconTemplate.png?asset';
+import { app } from 'electron';
 import { router } from './router';
+import { createTray } from './utils/createTray';
 
 app.whenReady().then(() => {
   // Set app user model ID for Windows
@@ -31,42 +29,3 @@ app.whenReady().then(() => {
 
   console.log(`Link listening on port ${import.meta.env.VITE_PORT}`);
 });
-
-const createTray = () => {
-  const icon = nativeImage.createFromPath(iconPath);
-
-  const tray = new Tray(icon);
-
-  tray.setToolTip('Dabase Link');
-
-  const contextMenuItems: MenuItemConstructorOptions[] = [
-    {
-      label: 'Quit',
-      click: app.quit,
-    },
-  ];
-
-  if (process.platform !== 'linux') {
-    contextMenuItems.unshift(
-      {
-        label: 'Start on Login',
-        type: 'checkbox',
-        checked: (settings.getSync('startOnLogin') as boolean | undefined) ?? false,
-        click: (menuItem) => {
-          const newValue = menuItem.checked;
-          settings.setSync('startOnLogin', newValue);
-          toggleAutoLaunch(newValue);
-        },
-      },
-      { type: 'separator' },
-    );
-  }
-
-  const contextMenu = Menu.buildFromTemplate(contextMenuItems);
-
-  tray.setContextMenu(contextMenu);
-};
-
-const toggleAutoLaunch = (enable: boolean) => {
-  app.setLoginItemSettings({ openAtLogin: enable });
-};
---
+++ link/src/main/utils/createTray.ts
@@ -0,0 +1,42 @@
+import type { MenuItemConstructorOptions } from 'electron';
+import { Menu, Tray, app, nativeImage } from 'electron';
+import settings from 'electron-settings';
+import iconPath from '../../resources/trayIconTemplate.png?asset';
+
+/**
+ * Create icon in OS tray and set up context menu
+ */
+export const createTray = () => {
+  const icon = nativeImage.createFromPath(iconPath);
+
+  const tray = new Tray(icon);
+
+  tray.setToolTip('Dabase Link');
+
+  const contextMenuItems: MenuItemConstructorOptions[] = [
+    {
+      label: 'Quit',
+      click: app.quit,
+    },
+  ];
+
+  if (process.platform !== 'linux') {
+    contextMenuItems.unshift(
+      {
+        label: 'Start on Login',
+        type: 'checkbox',
+        checked: (settings.getSync('startOnLogin') as boolean | undefined) ?? false,
+        click: (menuItem) => {
+          const newValue = menuItem.checked;
+          settings.setSync('startOnLogin', newValue);
+          app.setLoginItemSettings({ openAtLogin: newValue });
+        },
+      },
+      { type: 'separator' },
+    );
+  }
+
+  const contextMenu = Menu.buildFromTemplate(contextMenuItems);
+
+  tray.setContextMenu(contextMenu);
+};
