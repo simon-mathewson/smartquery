d7391401d528c67bce84f368e3e93c9cd5831b3d
Simon Mathewson <simon.mathewson@cosuno.de>
2024-06-15 21:28:50 +0200

    Set up component testing

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -7,7 +7,6 @@ import { LinkProvider } from './content/link/Provider';
 import { TabsProvider } from './content/tabs/Provider';
 import { QueriesProvider } from './content/tabs/Queries/Provider';
 import { useTheme } from './content/theme/useTheme';
-import './index.css';
 import { ClickOutsideQueueProvider } from './shared/hooks/useClickOutside/useQueue/Provider';
 import { TrpcProvider } from './content/trpc/Provider';
 import { ErrorBoundary } from './content/errorBoundary/ErrorBoundary';
