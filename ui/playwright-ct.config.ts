042e62041642e14cd7b0fa879ffcaa64e3d1bfd4
simon-mathewson <info@simonmathewson.com>
2024-10-26 13:31:02 +0200

    Enable retries in CI

--- ui/playwright-ct.config.ts
+++ ui/playwright-ct.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   timeout: 10 * 1000,
   fullyParallel: true,
   forbidOnly: process.env.CI !== undefined,
-  retries: 0,
+  retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI ? [['junit', { outputFile: 'test-ct-report.xml' }]] : 'html',
   use: {
