ff37ba846a1fcaf6bf8f9cf84e9ed2ad808aeff2
simon-mathewson <info@simonmathewson.com>
2024-10-27 12:49:45 +0100

    Fix import

--- ui/src/content/analytics/setUpAnalytics.ts
+++ ui/src/content/analytics/setUpAnalytics.ts
@@ -1,5 +1,5 @@
 import { Amplify } from 'aws-amplify';
-import Analytics from 'aws-amplify/analytics';
+import * as Analytics from 'aws-amplify/analytics';
 
 export const setUpAnalytics = () => {
   const { VITE_AWS_COGNITO_IDENTITY_POOL_ID, VITE_AWS_PINPOINT_APP_ID, VITE_AWS_REGION } =
