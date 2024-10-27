0c0b3c0886959f8463c8098a13cacececb01c47b
simon-mathewson <info@simonmathewson.com>
2024-10-27 14:21:51 +0100

    Identify guests and send locale

--- ui/src/content/analytics/setUpAnalytics.ts
+++ ui/src/content/analytics/setUpAnalytics.ts
@@ -1,5 +1,6 @@
 import { Amplify } from 'aws-amplify';
 import * as Analytics from 'aws-amplify/analytics';
+import { v4 as uuidv4 } from 'uuid';
 
 export const setUpAnalytics = () => {
   const { VITE_AWS_COGNITO_IDENTITY_POOL_ID, VITE_AWS_PINPOINT_APP_ID, VITE_AWS_REGION } =
@@ -35,5 +36,21 @@ export const setUpAnalytics = () => {
   Analytics.configureAutoTrack({
     enable: true,
     type: 'session',
+  });
+
+  const storedGuestId = localStorage.getItem('guestId');
+  const guestId = storedGuestId ?? uuidv4();
+
+  if (!storedGuestId) {
+    localStorage.setItem('guestId', guestId);
+  }
+
+  Analytics.identifyUser({
+    userId: guestId,
+    userProfile: {
+      demographic: {
+        locale: navigator.language,
+      },
+    },
   });
 };
