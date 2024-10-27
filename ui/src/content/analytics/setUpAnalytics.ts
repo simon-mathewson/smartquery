1daf2d15add5397efb4f94d6ca01f935fb12e223
simon-mathewson <info@simonmathewson.com>
2024-10-27 13:20:50 +0100

    Add guest access to Cognito in analytics setup

--- ui/src/content/analytics/setUpAnalytics.ts
+++ ui/src/content/analytics/setUpAnalytics.ts
@@ -4,6 +4,7 @@ import * as Analytics from 'aws-amplify/analytics';
 export const setUpAnalytics = () => {
   const { VITE_AWS_COGNITO_IDENTITY_POOL_ID, VITE_AWS_PINPOINT_APP_ID, VITE_AWS_REGION } =
     import.meta.env;
+  console.log({ VITE_AWS_COGNITO_IDENTITY_POOL_ID, VITE_AWS_PINPOINT_APP_ID, VITE_AWS_REGION });
 
   if (!VITE_AWS_COGNITO_IDENTITY_POOL_ID || !VITE_AWS_PINPOINT_APP_ID || !VITE_AWS_REGION) {
     if (import.meta.env.DEV) return;
@@ -20,6 +21,7 @@ export const setUpAnalytics = () => {
     },
     Auth: {
       Cognito: {
+        allowGuestAccess: true,
         identityPoolId: VITE_AWS_COGNITO_IDENTITY_POOL_ID,
       },
     },
