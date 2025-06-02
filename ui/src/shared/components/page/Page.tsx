dfc31719832ff958be0c6e17f169015be3653a31
simon-mathewson <info@simonmathewson.com>
2025-06-02 12:44:07 +0200

    Add space at bottom of page for user settings

--- ui/src/shared/components/page/Page.tsx
+++ ui/src/shared/components/page/Page.tsx
@@ -5,7 +5,7 @@ export const Page: React.FC<React.PropsWithChildren> = (props) => {
   const { children } = props;
 
   return (
-    <div className="mx-auto flex w-[356px] flex-col items-center gap-6 py-8">
+    <div className="mx-auto flex w-[356px] flex-col items-center gap-6 pb-16 pt-8">
       <Logo htmlProps={{ className: 'w-16' }} />
       {children}
     </div>
