4d2affacaee55392dd21e0f58a9b3cb7d3c0361a
simon-mathewson <info@simonmathewson.com>
2025-06-13 13:09:07 +0200

    Ensure deps are installed during migration

--- cloud/setup-production.sh
+++ cloud/setup-production.sh
@@ -12,13 +12,13 @@ urlencode() {
     done
 }
 
-pnpm install
-
 # Extract username and password from secret JSON
 DB_USERNAME=$(echo $DABASE_CLOUD_DB_SECRET | jq -r '.username')
 DB_PASSWORD=$(echo $DABASE_CLOUD_DB_SECRET | jq -r '.password')
 
 # Construct DATABASE_URL using the RDS endpoint and credentials
 export DATABASE_URL="postgresql://${DB_USERNAME}:$(urlencode $DB_PASSWORD)@${DABASE_CLOUD_DB_ENDPOINT}/dabase_cloud?schema=public"
+
+pnpm install
 
 pnpm start-production
