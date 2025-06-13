f3414aa30f25a444da86d44fcef24c4c51638ed5
simon-mathewson <info@simonmathewson.com>
2025-06-13 13:30:10 +0200

    Attempt to fix urlencode

--- cloud/scripts/setup-production.sh
+++ cloud/scripts/setup-production.sh
@@ -5,8 +5,7 @@ DB_USERNAME=$(echo $DABASE_CLOUD_DB_SECRET | jq -r '.username')
 DB_PASSWORD=$(echo $DABASE_CLOUD_DB_SECRET | jq -r '.password')
 
 # Construct DATABASE_URL using the RDS endpoint and credentials
-source scripts/urlencode.sh
-export DATABASE_URL="postgresql://${DB_USERNAME}:$(urlencode $DB_PASSWORD)@${DABASE_CLOUD_DB_ENDPOINT}/dabase_cloud?schema=public"
+export DATABASE_URL="postgresql://${DB_USERNAME}:$(./scripts/urlencode.sh "$DB_PASSWORD")@${DABASE_CLOUD_DB_ENDPOINT}/dabase_cloud?schema=public"
 
 pnpm install
 
--- cloud/scripts/urlencode.sh
+++ cloud/scripts/urlencode.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-urlencode() {
-    echo "$1" | awk 'BEGIN { for (i = 0; i <= 255; i++) ord[sprintf("%c", i)] = i }
-    {
-        for (i = 1; i <= length($0); i++) {
-            c = substr($0, i, 1)
-            if (c ~ /[a-zA-Z0-9.~_-]/) printf c
-            else printf "%%%02X", ord[c]
-        }
-    }'
-} 
+echo "$1" | awk 'BEGIN { for (i = 0; i <= 255; i++) ord[sprintf("%c", i)] = i }
+{
+    for (i = 1; i <= length($0); i++) {
+        c = substr($0, i, 1)
+        if (c ~ /[a-zA-Z0-9.~_-]/) printf c
+        else printf "%%%02X", ord[c]
+    }
+}'
