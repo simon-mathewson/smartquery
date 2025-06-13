1bb04de382b48da6f4faa08e9d8d4cabf7bbb274
simon-mathewson <info@simonmathewson.com>
2025-06-13 12:13:58 +0200

    Include dependencies in cloud build

--- cloud/rollup.config.js
+++ cloud/rollup.config.js
@@ -1,6 +1,8 @@
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import tsConfigPaths from 'rollup-plugin-tsconfig-paths';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'main.ts',
@@ -10,10 +12,12 @@ export default {
     sourcemap: true,
   },
   plugins: [
+    nodeResolve(),
     tsConfigPaths(),
     typescript({
       tsconfig: './tsconfig.json',
     }),
     commonjs(),
+    json(),
   ],
 };
