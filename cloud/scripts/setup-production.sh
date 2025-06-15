44b03a96ed2698e9c3bf1037bed7647a60e4ab44
Simon Mathewson <info@simonmathewson.com>
2025-06-15 02:49:40 +0200

    Set up JWT secret for prod

--- cloud/scripts/setup-production.sh
+++ cloud/scripts/setup-production.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# Extract username and password from secret JSON
-DB_USERNAME=$(echo $DABASE_CLOUD_DB_SECRET | jq -r '.username')
-DB_PASSWORD=$(echo $DABASE_CLOUD_DB_SECRET | jq -r '.password')
-
 # Construct DATABASE_URL using the RDS endpoint and credentials
 chmod +x ./scripts/build-database-url.sh
 export DATABASE_URL=$(./scripts/build-database-url.sh)
