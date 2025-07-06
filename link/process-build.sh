a00ffee5ce195d42f844927808dedcfff7c77165
Simon Mathewson <info@simonmathewson.com>
2025-07-06 17:47:19 +0200

    Remove link process build script

--- link/process-build.sh
+++
@@ -1,8 +0,0 @@
-mv dist dist-raw
-mkdir dist
-
-# Copy all files ending in .exe, .dmg, .deb to new build directory
-find dist-raw -type f \( -name "dabase-link*.exe" -o -name "dabase-link*.dmg" -o -name "dabase-link*.deb" \) -exec cp {} dist \;
-
-# Remove the dist-raw directory
-rm -rf dist-raw
