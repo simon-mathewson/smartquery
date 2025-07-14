beb4cebd783495d69ecc9596204d3368bacf6c7f
Simon Mathewson <info@simonmathewson.com>
2025-07-14 15:42:23 +0200

    Fix about meta tags

--- about/pages/_app.tsx
+++ about/pages/_app.tsx
@@ -1,19 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const title =
     "Dabase – AI-powered, browser-based database UI for Postgres, MySQL, and SQLite";
   const description =
     "AI-powered, browser-based database UI for Postgres, MySQL, and SQLite. The AI copilot can answer questions and generate SQL queries tailored to your database. View and edit your data in a user-friendly interface.";
-
-  const [win, setWin] = useState<Window | null>(null);
-
-  useEffect(() => {
-    setWin(window);
-  }, []);
 
   return (
     <>
@@ -23,16 +16,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-
-        {win && (
-          <>
-            <meta
-              property="og:image"
-              content={`${win.location.origin}/apple-touch-icon-180x180.png`}
-            />
-            <meta property="og:url" content={win.location.origin} />
-          </>
-        )}
+        <meta
+          property="og:image"
+          content="https://about.dabase.dev/apple-touch-icon-180x180.png"
+        />
+        <meta property="og:url" content="https://about.dabase.dev" />
       </Head>
       <Component {...pageProps} />
     </>
