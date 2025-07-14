b0f072d9c6af68b0f5a34c9a5d6742fb65a23a3e
Simon Mathewson <info@simonmathewson.com>
2025-07-14 15:48:41 +0200

    Update og logo

--- about/pages/_app.tsx
+++ about/pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   const title =
     "Dabase – AI-powered, browser-based database UI for Postgres, MySQL, and SQLite";
   const description =
-    "AI-powered, browser-based database UI for Postgres, MySQL, and SQLite. The AI copilot can answer questions and generate SQL queries tailored to your database. View and edit your data in a user-friendly interface.";
+    "The AI copilot can answer questions and generate SQL queries tailored to your database. View and edit your data in a user-friendly interface.";
 
   return (
     <>
@@ -16,10 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta
-          property="og:image"
-          content="https://about.dabase.dev/apple-touch-icon-180x180.png"
-        />
+        <meta property="og:image" content="https://about.dabase.dev/logo.png" />
         <meta property="og:url" content="https://about.dabase.dev" />
       </Head>
       <Component {...pageProps} />
