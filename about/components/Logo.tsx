e2203b8970bc703bf3e5195acf97f52074767e19
Simon Mathewson <info@simonmathewson.com>
2025-07-14 15:14:37 +0200

    Update about logo, meta tags

--- about/components/Logo.tsx
+++ about/components/Logo.tsx
@@ -3,11 +3,8 @@ import Link from "next/link";
 
 export const Logo: React.FC = () => {
   return (
-    <Link href="/" className="flex gap-5 justify-center items-center py-6">
-      <Image src="/logo.svg" alt="" width={48} height={48} />
-      <h1 className="text-5xl font-[700] text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-green-500 from-20% to-80%">
-        Dabase
-      </h1>
+    <Link href="/" className="mt-8 mb-4 h-10 !block relative">
+      <Image src="/logo.svg" alt="" fill />
     </Link>
   );
 };
--- about/pages/_app.tsx
+++ about/pages/_app.tsx
@@ -1,18 +1,33 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useEffect, useRef } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const title =
+    "Dabase – AI-powered, browser-based database UI for Postgres, MySQL, and SQLite";
+  const description =
+    "AI-powered, browser-based database UI for Postgres, MySQL, and SQLite. The AI copilot can answer questions and generate SQL queries tailored to your database. View and edit your data in a user-friendly interface.";
+
+  const windowRef = useRef<Window | null>(null);
+
+  useEffect(() => {
+    windowRef.current = window;
+  }, []);
+
   return (
     <>
       <Head>
-        <title>
-          Dabase &ndash; The fun, browser-based, AI-powered database UI
-        </title>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
         <meta
-          name="description"
-          content="The fun, browser-based, AI-powered database UI for PostgreSQL, MySQL, and SQLite."
+          property="og:image"
+          content={`${windowRef.current?.location.origin}/apple-touch-icon-180x180.png`}
         />
+        <meta property="og:url" content={windowRef.current?.location.origin} />
       </Head>
       <Component {...pageProps} />
     </>
