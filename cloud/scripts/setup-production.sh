da8674791db796b277310bf84107221098dadccb
Simon Mathewson <info@simonmathewson.com>
2025-07-24 22:08:49 +0200

    Attempt to fix Prisma binaries location

--- cloud/scripts/setup-production.sh
+++ cloud/scripts/setup-production.sh
@@ -6,6 +6,9 @@ set -e
 chmod +x ./scripts/build-database-url.sh
 export DATABASE_URL=$(./scripts/build-database-url.sh)
 
+# Move all *.node files to root of the app
+mv dist/*.node .
+
 set -a
 source .env.production
 set +a
