1bee254b0d5de451dd48c101cf9eea4059b5bb76
Simon Mathewson <info@simonmathewson.com>
2025-09-03 17:50:22 +0200

    Improve contrast of data type coloring

--- ui/src/content/tabs/queries/query/table/cell/Cell.tsx
+++ ui/src/content/tabs/queries/query/table/cell/Cell.tsx
@@ -190,6 +190,7 @@ export const Cell: React.FC<CellProps> = (props) => {
               value === undefined ||
               (typeof column === 'object' &&
                 (column.isUnique ||
+                  column.foreignKey ||
                   column.isPrimaryKey ||
                   ['boolean', 'json'].includes(column.dataType) ||
                   isDateOrTimeType(column.dataType) ||
@@ -198,16 +199,16 @@ export const Cell: React.FC<CellProps> = (props) => {
           ...(type === 'body' &&
             typeof column === 'object' &&
             !selected && {
-              'text-emerald-600 dark:text-emerald-500':
+              'text-emerald-700 dark:text-emerald-500':
                 !isNil(value) && isNumberType(column.dataType),
-              'text-amber-600 dark:text-amber-500':
+              'text-amber-700 dark:text-amber-500':
                 !isNil(value) && isDateOrTimeType(column.dataType),
-              '!text-blue-600 dark:!text-blue-500': !isNil(value) && column.isPrimaryKey,
-              '!text-sky-600 dark:!text-sky-500':
+              '!text-blue-700 dark:!text-blue-500': !isNil(value) && column.isPrimaryKey,
+              '!text-sky-700 dark:!text-sky-500':
                 !isNil(value) && !column.isPrimaryKey && column.foreignKey !== null,
-              '!text-indigo-600 dark:!text-indigo-500': !isNil(value) && column.isUnique,
-              'text-teal-600 dark:text-teal-500': !isNil(value) && column.dataType === 'boolean',
-              'text-fuchsia-600 dark:text-fuchsia-500':
+              '!text-indigo-700 dark:!text-indigo-500': !isNil(value) && column.isUnique,
+              'text-teal-700 dark:text-teal-500': !isNil(value) && column.dataType === 'boolean',
+              'text-fuchsia-700 dark:text-fuchsia-500':
                 !isNil(value) && isEnumType(column.dataType),
             }),
           'text-textTertiary':
