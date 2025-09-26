851c7afaf76a58320b5a8b745ba6a43372cec591
Simon Mathewson <info@simonmathewson.com>
2025-09-26 19:45:16 +0200

    Add node-ssh-forward to link deps

--- shared/connector/createSshTunnel.ts
+++ shared/connector/createSshTunnel.ts
@@ -1,15 +1,15 @@
-import { SSHConnection } from "node-ssh-forward";
-import findFreePorts from "find-free-ports";
+import { SSHConnection } from 'node-ssh-forward';
+import findFreePorts from 'find-free-ports';
 
-import type { RemoteConnection } from "@/connections/types";
-import assert from "assert";
+import type { RemoteConnection } from '@/connections/types';
+import assert from 'assert';
 
 export const createSshTunnel = async (connection: RemoteConnection) => {
   const { host, port, ssh } = connection;
 
   assert(ssh);
 
-  const sshLocalHost = "localhost";
+  const sshLocalHost = 'localhost';
   const [sshLocalPort] = await findFreePorts(1, { startPort: 49152 });
 
   const sshTunnel = new SSHConnection({
