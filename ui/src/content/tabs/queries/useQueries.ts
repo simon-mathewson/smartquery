7078f647187e9c487d7a7ea1ba4705eefddb53b7
Simon Mathewson <info@simonmathewson.com>
2025-09-26 14:35:49 +0200

    Fix deduplicating of columns

--- ui/src/content/tabs/queries/useQueries.ts
+++ ui/src/content/tabs/queries/useQueries.ts
@@ -127,7 +127,7 @@ export const useQueries = () => {
         // If there are multiple columns with the same name, we only keep the first.
         // This can happen if all columns of multiple tables are selected.
         // Prefer visible columns.
-        const tableColumns = uniqBy(sortBy(columnsWithDuplicates, 'name'), 'isVisible');
+        const tableColumns = uniqBy(sortBy(columnsWithDuplicates, 'isVisible'), 'name');
         const virtualColumns = getVirtualColumns(
           firstSelectResult,
           tableColumns.map((column) => column.name),
