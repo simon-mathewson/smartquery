3a60edf7fca094293881330cf9d1056830306d56
Simon Mathewson <info@simonmathewson.com>
2025-10-15 14:37:21 +0200

    Update meta description

--- about/pages/_app.tsx
+++ about/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   const title =
     "SmartQuery – AI-powered, browser-based database UI for Postgres, MySQL, and SQLite";
   const description =
-    "The AI copilot can answer questions and generate SQL queries tailored to your database. View and edit your data in a user-friendly interface.";
+    "Query, visualize, and modify your database using natural language. Get tailor-made queries from a schema-aware AI assistant. Insert, update, and delete with ease. Bring your data to life with charts. Ask your database anything. Intellisense and inline completions. Thoughtful, customizable design. Access your data from anywhere.";
 
   return (
     <>
--- ui/App.tsx
+++ ui/App.tsx
@@ -8,7 +8,7 @@ import { Router } from './router/router';
 
 export const App: React.FC = () => {
   const description =
-    'AI-powered, browser-based database UI for Postgres, MySQL, and SQLite. The AI copilot can answer questions and generate SQL queries tailored to your database. View and edit your data in a user-friendly interface.';
+    'AI-powered, browser-based database UI for Postgres, MySQL, and SQLite. Query, visualize, and modify your database using natural language. Get tailor-made queries from a schema-aware AI assistant. Insert, update, and delete with ease. Bring your data to life with charts. Ask your database anything. Intellisense and inline completions. Thoughtful, customizable design. Access your data from anywhere.';
 
   return (
     <>
