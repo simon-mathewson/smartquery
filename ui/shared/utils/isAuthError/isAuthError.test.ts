d5f31bf97181071cd2f17c6b25c092f827f01b31
Simon Mathewson <info@simonmathewson.com>
2025-10-22 20:42:23 +0200

    Update `isAuthError`

--- ui/shared/utils/isAuthError/isAuthError.test.ts
+++ ui/shared/utils/isAuthError/isAuthError.test.ts
@@ -2,8 +2,13 @@ import { expect, test } from 'vitest';
 import { isAuthError } from './isAuthError';
 
 test('isAuthError', () => {
-  expect(isAuthError(new Error('Authentication failed'))).toBe(true);
+  expect(isAuthError(new Error('Authentication failed'))).toBe(false);
   expect(isAuthError(new Error('Failed to authenticate'))).toBe(false);
-  expect(isAuthError(new Error('Authentication failed: invalid password'))).toBe(true);
+  expect(isAuthError(new Error('Authentication failed: invalid password'))).toBe(false);
   expect(isAuthError(new Error('Failed to authenticate: invalid password'))).toBe(false);
+  expect(
+    isAuthError(new Error("Access denied for user 'root'@'192.168.65.1' (using password: YES)")),
+  ).toBe(true);
+  expect(isAuthError(new Error('All configured authentication methods failed'))).toBe(true);
+  expect(isAuthError(new Error('password authentication failed for user '))).toBe(true);
 });
--- ui/shared/utils/isAuthError/isAuthError.ts
+++ ui/shared/utils/isAuthError/isAuthError.ts
@@ -1,2 +1,6 @@
 export const isAuthError = (error: unknown) =>
-  error instanceof Error && error.message.includes('Authentication failed');
+  error instanceof Error &&
+  (error.message.startsWith('Access denied for user ') ||
+    error.message.startsWith('Cannot parse privateKey: ') ||
+    error.message.startsWith('password authentication failed for user ') ||
+    error.message === 'All configured authentication methods failed');
