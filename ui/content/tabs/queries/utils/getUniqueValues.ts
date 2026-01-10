b8c70115aa22a1e27a126f46554c5b1ce3bd3705
Simon Mathewson <info@simonmathewson.com>
2026-01-10 21:03:10 +0100

    Fix unique values being taken from wrong column index

--- ui/content/tabs/queries/utils/getUniqueValues.ts
+++ ui/content/tabs/queries/utils/getUniqueValues.ts
@@ -1,12 +1,12 @@
 import type { Column } from '~/shared/types';
-import type { DbValue } from '@/connector/types';
+import type { DbValue } from '@/native/types';
 
 export const getUniqueValues = (columns: Column[], row: DbValue[] | undefined) => {
   if (!row) return null;
 
   const uniqueColumns = columns
-    .filter((column, index) => (column.isPrimaryKey || column.isUnique) && index in row)
-    .map((column, index) => ({ column, index }));
+    .map((column, index) => ({ column, index }))
+    .filter(({ column, index }) => (column.isPrimaryKey || column.isUnique) && index in row);
 
   return uniqueColumns.map(({ column, index }) => ({
     column: column.originalName,
