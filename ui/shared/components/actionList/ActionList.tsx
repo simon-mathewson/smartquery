46e37eaa9f592420981f89eb236430a1f5318b86
Simon Mathewson <info@simonmathewson.com>
2026-01-14 16:15:15 +0100

    Fix action list spacing

--- ui/shared/components/actionList/ActionList.tsx
+++ ui/shared/components/actionList/ActionList.tsx
@@ -30,7 +30,7 @@ export const ActionList: React.FC<ActionListProps> = (props) => {
               // eslint-disable-next-line @typescript-eslint/no-explicit-any
               ...(action.htmlProps as any),
               className: classNames(
-                'flex !min-h-[56px] cursor-pointer items-center gap-3 overflow-hidden focus:!bg-card hover:!bg-card focus:!bg-background !text-textPrimary !justify-start !rounded-2xl border border-border bg-card px-2 py-2 text-left shadow-2xl hover:border-borderHover',
+                'flex !min-h-[56px] cursor-pointer items-center gap-4 overflow-hidden focus:!bg-card hover:!bg-card focus:!bg-background !text-textPrimary !justify-start !rounded-2xl border border-border bg-card !pl-4 pr-2 py-2 text-left shadow-2xl hover:border-borderHover',
                 {
                   '!min-h-[40px] !gap-2 !px-2': compact,
                 },
